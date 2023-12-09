98e8114b56537f300b16dfddc1e1517da70bff06
cloud-network-guy <greenlakejohnny@gmail.com>
2023-12-09 15:13:14 -0800

    fix

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,7 @@ output "backend_services" {
     {
       index_key = v.index_key
       id        = v.is_regional ? google_compute_region_backend_service.default[v.index_key].id : google_compute_backend_service.default[v.index_key].id
+      name      = v.name
       type      = v.type
       region    = v.is_regional ? lookup(v, "region", "error") : "global"
       groups    = v.groups
