5f9d9150e21d3713ca0cb379db0f4a59da6e4027
cloud-network-guy <greenlakejohnny@gmail.com>
2023-12-09 13:16:14 -0800

    initial push

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ output "backend_services" {
       index_key = v.index_key
       id        = v.is_regional ? google_compute_region_backend_service.default[v.index_key].id : google_compute_backend_service.default[v.index_key].id
       type      = v.type
-      region    = local.is_regional ? lookup(v, "region", "error") : "global"
+      region    = v.is_regional ? lookup(v, "region", "error") : "global"
       groups    = v.groups
     }
   ]
